742992d2fea06486a4cf27d0684332bba730fba1
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2025-10-27 17:31:47 -0400

    split route

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import App1 from './App1';
+import App2 from './App2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />
+root.render(
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App1 />} />
+      <Route path="/1" element={<App1 />} />
+      <Route path="/2" element={<App2 />} />
+    </Routes>
+  </HashRouter>
+
 );
