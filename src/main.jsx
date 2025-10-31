d117c450b5b7e007f1e8ec7f94082d445bc6f646
Dave Seidman <daveseidman@Daves-MacBook-Pro.local>
2025-10-31 12:58:58 -0400

    pick better model

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import App1 from './App1';
-import App2 from './App2';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <HashRouter>
-    <Routes>
-      <Route path="/" element={<App1 />} />
-      <Route path="/1" element={<App1 />} />
-      <Route path="/2" element={<App2 />} />
-    </Routes>
-  </HashRouter>
-
-);
+root.render(<App />);
